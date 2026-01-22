6ade45b25669b8d37b3ac7b43c51990969522855
ja0nz <git@ja.nz>
2026-01-22 22:30:02 +0100

    feat: added cloudflare dyndns

--- modules/ddclient.nix
+++ modules/ddclient.nix
@@ -6,7 +6,7 @@
 }:
 
 {
-  sops.secrets."dyndns_namecheap_token" = { };
+  sops.secrets."cloudflare_namecheap_token" = { };
 
   # https://search.nixos.org/options?query=ddclient
   # https://github.com/ddclient/ddclient/blob/main/ddclient.conf.in
@@ -14,13 +14,13 @@
     enable = true;
     usev4 = "webv4, webv4=api.ipify.org, webv4=ipv4.icanhazip.com";
     usev6 = "webv6, webv6=api6.ipify.org, webv6=icanhazip.com";
-    protocol = "namecheap";
-    server = "dynamicdns.park-your-domain.com";
-    username = "${vars.rootDomain}";
-    passwordFile = config.sops.secrets."dyndns_namecheap_token".path;
+    protocol = "cloudflare";
+    zone = "${vars.rootDomain}";
+    userName = "token";
+    passwordFile = config.sops.secrets."cloudflare_namecheap_token".path;
     domains = [
-      "@"
-      "*"
+      "${vars.rootDomain}"
+      "*.${vars.rootDomain}"
     ];
   };
 }
